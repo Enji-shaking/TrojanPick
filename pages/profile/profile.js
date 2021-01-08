b6ac4fb5023db07f21394a341cd642f14b38dd7b
Enji-shaking <enjili@usc.edu>
2021-01-08 16:14:19 +0800

    working login

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -7,68 +7,148 @@ Page({
    * 页面的初始数据
    */
   data: {
-    nickName: "",
-    avatarUrl: "",
-    userID:"oH5r15EPI59JgaNhhebzuDsOpPEo"
+    userName: "",
+    avatarUrl: "/icon/ours/empty_icon.png",
+    openID:"",
+    hasPersonalInfo: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(app.globalData);
+    wx.showLoading({
+      title: 'loading',
+    })
+    console.log("hey");
+    console.log("hey2");
+
+    if(this.data.openID === ""){
+      const data = wx.getStorageSync("openID");
+      console.log(data);
+      this.setData({openID: data})
+    }
+
+    if(!this.data.hasPersonalInfo){
+      wx.getStorage({
+        key: "userInfo",
+        success: (res)=>{
+          console.log(res);
+          //set data
+          this.setData({
+            hasPersonalInfo: true,
+            avatarUrl: res.data.avatarUrl,
+            userName: res.data.userName
+          })
+        },
+        fail: e=>{
+          this.getPersonalInfoFromDatabase()
+        },
+        complete(c){
+          wx.hideLoading()
+        }
+      });
+    }
+  },
+  
+  //print a placeholder
+    //the placeholder have a button
+    //if the user allows, insert userinfo into database, save it locally, goto **
+
+  //**check if we have personal info locally
+    //if we do, print data, allow the user to reload personal info
+      //if the user reload data, update database, update local data
+  //if we don't have one locally
+    //go check if we have user info in the database
+      //if we do, pull it to locally, go to the 
+      
+      //if we don't, do nothing, go to *** 
+      
+  processUserInfo(rawData, type){
+    const data = JSON.parse(rawData)
+    console.log(data);
+    //1. set user info
+    wx.setStorage({
+      key: 'userInfo',
+      data: {
+        hasPersonalInfo: true,
+        avatarUrl: data.avatarUrl,
+        userName: data.userName
+      },
+      success: (result) => {
+        console.log(result);
+      },
+      fail: (err) => {console.log(err);}
+    });
+    //2. insert to database
+    wx.cloud.callFunction({
+      name: "userRelatedFn",
+      data: {
+        target: type==="add"?"addNewUser":"updateUser",
+        avatarUrl: data.avatarUrl,
+        userName: data.userName,
+        openID: this.data.openID,
+      },
+    })
+    //3. set page info
     this.setData({
-      nickName: app.globalData.nickName,
-      avatarUrl: app.globalData.avatarUrl
+      hasPersonalInfo: true,
+      avatarUrl: data.avatarUrl,
+      userName: data.userName
+    });
+  },
+  onGetUserInfoNewUser(e){
+    //would only be triggered when we don't have userInfo in the storage, neither did we get any information from the database
+    console.log(e);
+    processUserInfo(e.detail, "add")
+  },
+  onTapUpdateInfo(){
+    wx.getUserInfo({
+      withCredentials: 'false',
+      lang: 'zh_CN',
+      timeout:10000,
+      success: (result) => {
+        this.processUserInfo(result.rawData);
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+  },
+  getPersonalInfoFromDatabase(){
+    wx.cloud.callFunction({
+      name: "userRelatedFn",
+      data: {
+        target: "checkUserInfo",
+        openID: this.data.openID
+      },
+      success: (e)=>{
+        console.log(e);
+        const personalInfo = e.result.data
+        if(personalInfo.length > 0){
+          this.setData({
+            hasPersonalInfo: true,
+            avatarUrl: personalInfo[0].avatarUrl,
+            userName: personalInfo[0].userName
+          });
+          wx.setStorage({
+            key: 'userInfo',
+            data: {
+              hasPersonalInfo: true,
+              avatarUrl: personalInfo[0].avatarUrl,
+              userName: personalInfo[0].userName
+            },
+            success: (result) => {
+              console.log(result);
+            },
+            fail: (err) => {console.log(err);}
+          });
+            
+        }
+      },
+      fail: (e)=>{
+        console.log("fail");
+        console.log(e);
+      }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
