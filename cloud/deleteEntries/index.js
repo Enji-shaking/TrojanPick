4a7c263d4c3ee6db5b27409d241375be4a528bcc
Enji-shaking <enjili@usc.edu>
2021-01-14 17:22:20 +0800

    Fxied delete entries. (Fucking zeros)

--- cloud/deleteEntries/index.js
+++ cloud/deleteEntries/index.js
@@ -46,60 +46,129 @@ exports.main = async (event, context) => {
     }).get()
     console.log(course_prof_data);
     let numReviews_orig = course_prof_data.data[0].numReviews;
+    if(!numReviews_orig) numReviews_orig = 0
     let workloadRating_orig = course_prof_data.data[0].workloadRating;
     let difficultyRating_orig = course_prof_data.data[0].difficultyRating;
     let interestingRating_orig = course_prof_data.data[0].interestingRating;
     let teachingRating_orig = course_prof_data.data[0].teachingRating;
-    await db.collection('course_professor').doc(course_prof_data.data[0]._id).update({
-      data: {
-        workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
-        interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
-        teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
-        difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
-        numReviews: numReviews_orig - 1
-      }
-    })
-    console.log("I'm here2");
 
+    if(numReviews_orig === 1){
+      db.collection('course_professor')
+      // .doc(course_prof_data.data[0]._id)
+      .where({
+        courseID: courseID,
+        professorID: professorID
+      })
+      .update({
+        data: {
+          workloadRating: 0,
+          interestingRating: 0,
+          teachingRating: 0,
+          difficultyRating: 0,
+          numReviews: 0
+          
+        }
+      })
+    }else{
+      db.collection('course_professor')
+        // .doc(course_prof_data.data[0]._id)
+        .where({
+          courseID: courseID,
+          professorID: professorID
+        })
+        .update({
+          data: {
+            workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
+            interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
+            teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
+            difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
+            numReviews: numReviews_orig - 1
+            
+          }
+        })
+    }
     // update averages in 'courses'
     let course_data = await db.collection('courses').where({
       _id: courseID
     }).get();
     console.log(course_data);
     numReviews_orig = course_data.data[0].numReviews;
+    // if(!numReviews_orig) numReviews_orig = 0
     workloadRating_orig = course_data.data[0].workloadRating;
     difficultyRating_orig = course_data.data[0].difficultyRating;
     interestingRating_orig = course_data.data[0].interestingRating;
     teachingRating_orig = course_data.data[0].teachingRating;
-    await db.collection('courses').doc(courseID).update({
-      data: {
-        workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
-        interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
-        teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
-        difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
-        numReviews: numReviews_orig - 1
-      }
-    })
-    console.log("I'm here3");
-
+    if(numReviews_orig === 1){
+      db.collection('courses')
+      // .doc(courseID)
+      .where({
+        _id: courseID
+      })
+      .update({
+        data: {
+          workloadRating: 0,
+          interestingRating: 0,
+          teachingRating: 0,
+          difficultyRating: 0,
+          numReviews: 0
+        }
+      })
+    }else{
+      db.collection('courses')
+        // .doc(courseID)
+        .where({
+          _id: courseID
+        })
+        .update({
+          data: {
+            workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
+            interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
+            teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
+            difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
+            numReviews: numReviews_orig - 1
+          }
+        })
+    }
+  
     // update averages in 'professors'
     let prof_data = await db.collection('professors').where({
       _id: professorID
     }).get();
     numReviews_orig = prof_data.data[0].numReviews;
+    if(!numReviews_orig) numReviews_orig = 1
     workloadRating_orig = prof_data.data[0].workloadRating;
     difficultyRating_orig = prof_data.data[0].difficultyRating;
     interestingRating_orig = prof_data.data[0].interestingRating;
     teachingRating_orig = prof_data.data[0].teachingRating;
-    await db.collection('professors').doc(professorID).update({
-      data: {
-        workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
-        interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
-        teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
-        difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
-        numReviews: numReviews_orig - 1
-      }
-    })
+    
+    if(numReviews_orig === 1){
+      db.collection('professors')
+      .doc(professorID)
+      .update({
+        data: {
+          workloadRating: 0,
+          interestingRating: 0,
+          teachingRating: 0,
+          difficultyRating: 0,
+          numReviews: 0
+        }
+      })
+    }else{
+      db.collection('professors')
+        // .doc(professorID)
+        .where({
+          _id: professorID
+        })
+        .update({
+          data: {
+            workloadRating: (workloadRating_orig * numReviews_orig - workloadRating) / (numReviews_orig - 1),
+            interestingRating: (interestingRating_orig * numReviews_orig - interestingRating) / (numReviews_orig - 1),
+            teachingRating: (teachingRating_orig * numReviews_orig - teachingRating) / (numReviews_orig - 1),
+            difficultyRating: (difficultyRating_orig * numReviews_orig - difficultyRating) / (numReviews_orig - 1),
+            numReviews: numReviews_orig - 1
+          }
+        })
+    }
 
   }else if(target === "deteleQuestion"){
     db.collection("questions").where({
